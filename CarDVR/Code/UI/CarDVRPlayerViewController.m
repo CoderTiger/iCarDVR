1ac2ec8a4ccafc205bb084a021baa8c7c8e30fef
CoderTiger <EastdawnYang@gmail.com>
2013-11-04 18:11:03 +0800

    recorded video can be played now.

--- CarDVR/Code/UI/CarDVRPlayerViewController.m
+++ CarDVR/Code/UI/CarDVRPlayerViewController.m
@@ -8,10 +8,16 @@
 
 #import "CarDVRPlayerViewController.h"
 #import "CarDVRVideoItem.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface CarDVRPlayerViewController ()
 
 @property (weak, nonatomic) CarDVRVideoItem *videoItem;
+@property (strong, nonatomic) MPMoviePlayerController *playerController;
+@property (weak, nonatomic) IBOutlet UIView *playerProtraitPaneView;
+
+#pragma mark - private methods
+- (void)installPlayerControllerWithContentURL:(NSURL *)aURL;
 
 @end
 
@@ -35,12 +41,34 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    NSURL *videoURL = [NSURL fileURLWithPath:_videoItem.filePath];
+    [self installPlayerControllerWithContentURL:videoURL];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)installPlayerControllerWithContentURL:(NSURL *)aURL
+{
+    if ( _playerController )
+    {
+        return;
+    }
+    _playerController = [[MPMoviePlayerController alloc] initWithContentURL:aURL];
+    _playerController.controlStyle = MPMovieControlStyleEmbedded;
+    _playerController.shouldAutoplay = NO;
+    [_playerController prepareToPlay];
+    CGRect viewBounds = self.view.bounds;
+    CGRect playerPortraitFrame = self.playerProtraitPaneView.frame;
+    CGRect playerFrame = CGRectMake( viewBounds.origin.x,
+                                    viewBounds.origin.y,
+                                    playerPortraitFrame.size.width,
+                                    playerPortraitFrame.size.height );
+    [_playerController.view setFrame:playerFrame];
+    [self.view addSubview:_playerController.view];
 }
 
 @end
