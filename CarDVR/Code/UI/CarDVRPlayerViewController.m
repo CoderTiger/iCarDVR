5e07752b5968f225673627ab2460072500e05742
CoderTiger <EastdawnYang@gmail.com>
2013-11-05 11:16:15 +0800

    fixed an issue on exiting player from fullscreen.

--- CarDVR/Code/UI/CarDVRPlayerViewController.m
+++ CarDVR/Code/UI/CarDVRPlayerViewController.m
@@ -18,6 +18,9 @@
 
 #pragma mark - private methods
 - (void)installPlayerControllerWithContentURL:(NSURL *)aURL;
+- (void)layoutSubviews;
+
+- (void)handleMPMoviePlayerDidExitFullscreenNotification;
 
 @end
 
@@ -37,12 +40,24 @@
     return self;
 }
 
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.navigationController.navigationBar.translucent = NO;
     NSURL *videoURL = [NSURL fileURLWithPath:_videoItem.filePath];
     [self installPlayerControllerWithContentURL:videoURL];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+#pragma unused( animated )
+    [self layoutSubviews];
 }
 
 - (void)didReceiveMemoryWarning
@@ -51,6 +66,40 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    [UIView animateWithDuration:duration animations:^{
+        if ( UIInterfaceOrientationIsLandscape( toInterfaceOrientation ) )
+        {
+            CGSize size = [UIScreen mainScreen].bounds.size;
+            size = CGSizeMake( size.height, size.width );
+            UIApplication *application = [UIApplication sharedApplication];
+       
+            if ( application.statusBarHidden == NO )
+            {
+                size.height -= MIN( application.statusBarFrame.size.width, application.statusBarFrame.size.height );
+            }
+            
+            CGRect bounds = self.view.bounds;
+            self.playerController.view.frame = CGRectMake( bounds.origin.x,
+                                                          bounds.origin.y,
+                                                          size.width,
+                                                          size.height );
+        }
+        else
+        {
+            self.playerController.view.frame = self.playerProtraitPaneView.frame;
+        }
+    }];
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+#pragma unused( fromInterfaceOrientation )
+    [self layoutSubviews];
+}
+
+#pragma mark - private methods
 - (void)installPlayerControllerWithContentURL:(NSURL *)aURL
 {
     if ( _playerController )
@@ -61,14 +110,32 @@
     _playerController.controlStyle = MPMovieControlStyleEmbedded;
     _playerController.shouldAutoplay = NO;
     [_playerController prepareToPlay];
-    CGRect viewBounds = self.view.bounds;
-    CGRect playerPortraitFrame = self.playerProtraitPaneView.frame;
-    CGRect playerFrame = CGRectMake( viewBounds.origin.x,
-                                    viewBounds.origin.y,
-                                    playerPortraitFrame.size.width,
-                                    playerPortraitFrame.size.height );
-    [_playerController.view setFrame:playerFrame];
+    [_playerController.view setFrame:self.playerProtraitPaneView.frame];
     [self.view addSubview:_playerController.view];
+    
+    NSNotificationCenter *defaultNC = [NSNotificationCenter defaultCenter];
+    [defaultNC addObserver:self
+                  selector:@selector(handleMPMoviePlayerDidExitFullscreenNotification)
+                      name:MPMoviePlayerDidExitFullscreenNotification
+                    object:_playerController];
+}
+
+- (void)layoutSubviews
+{
+    UIInterfaceOrientation statusBarOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    if ( UIInterfaceOrientationIsLandscape( statusBarOrientation ) )
+    {
+        self.playerController.view.frame = self.view.bounds;
+    }
+    else
+    {
+        self.playerController.view.frame = self.playerProtraitPaneView.frame;
+    }
+}
+
+- (void)handleMPMoviePlayerDidExitFullscreenNotification
+{
+    [self layoutSubviews];
 }
 
 @end
