0e3f94e25572ddb1cc81f5fd7f711f25bf607e0f
CoderTiger <EastdawnYang@gmail.com>
2014-03-10 17:43:09 +0800

    [1.0 Beta 2]minor change: fixed a warning.

--- CarDVR/Code/UI/CarDVRVideoDetailViewController.m
+++ CarDVR/Code/UI/CarDVRVideoDetailViewController.m
@@ -32,7 +32,6 @@
     if ( _videoItem )
     {
         self.title = [NSString stringWithFormat:NSLocalizedString( @"playerViewTitleFormat", nil ), _videoItem.videoFileName];
-        _videoItem.locations;
     }
 }
 
